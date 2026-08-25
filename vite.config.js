cb3176c9f0ae36def8a39023bab4490c83c1e4c1
Pol-zoom <dude121211@tutamail.com>
2026-08-25 15:10:27 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/birthday-again-surprise/",
+  base: "/birthday-ririru-surprise/",
 
   build: {
     outDir: "dist",
